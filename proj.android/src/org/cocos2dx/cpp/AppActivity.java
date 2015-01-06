9ccac11c68e08dbfe833fd316f0052e0301d25ec
Hans Zhang <ezburg@gmail.com>
2015-01-06 15:35:47 +0100

    Integrating the Admob in Android version

--- proj.android/src/org/cocos2dx/cpp/AppActivity.java
+++ proj.android/src/org/cocos2dx/cpp/AppActivity.java
@@ -28,5 +28,55 @@ package org.cocos2dx.cpp;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.app4cn.eatnum.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
 public class AppActivity extends Cocos2dxActivity {
+	private AdView adView;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// 创建adView。
+		adView = new AdView(this);
+		adView.setAdUnitId("ca-app-pub-5072970286349933/3457682424");
+		adView.setAdSize(AdSize.BANNER);
+
+		// 查询LinearLayout，假设其已指定
+		// 属性android:id="@+id/mainLayout"。
+		LinearLayout layout = (LinearLayout)findViewById(R.id.mainLayout);
+
+		// 在其中添加adView。
+		layout.addView(adView);
+
+		// 启动一般性请求。
+		AdRequest adRequest = new AdRequest.Builder().build();
+
+		// 在adView中加载广告请求。
+		adView.loadAd(adRequest);
+	}
+
+	@Override
+	public void onPause() {
+		adView.pause();
+		super.onPause();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		adView.resume();
+	}
+
+	@Override
+	public void onDestroy() {
+		adView.destroy();
+		super.onDestroy();
+	}
 }
